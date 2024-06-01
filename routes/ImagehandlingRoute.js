c2f0d2a9d935bb8a3705266407c161ac1b57fa14
ShihabMahmod <58717265+ShihabMahmod@users.noreply.github.com>
2024-06-01 08:10:25 +0600

    upload image on cloudinery

--- routes/ImagehandlingRoute.js
+++ routes/ImagehandlingRoute.js
@@ -3,43 +3,59 @@ import multer from "multer";
 import path from "path";
 import { Router } from "express";
 import axios from "axios";
+import {v2 as cloudinary} from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
 
 const router = Router();
 
 router.use(express.static("public"));
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const app = express();
+  cloudinary.config({ 
+    cloud_name: "dlg2emvsz", 
+    api_key: "448797641344156", 
+    api_secret: "De_TouHvQDd0xmGDbQyTeI0W0co"
+  });
 
-app.use(express.static("public"));
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "/tmp");
-  },
-  filename: function (req, file, cb) {
-    const name = Date.now() + "-" + file.originalname;
-    cb(null, name);
-  },
-});
+  const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: 'uploads', 
+      allowed_formats: ['jpg', 'jpeg', 'png', 'gif'],
+    },
+  });
+  const upload = multer({ storage: storage });
+  router.post("/", upload.single("icon"), async (req, res) => {
 
-const upload = multer({ storage: storage });
+    try {
 
-router.post("/", upload.single("icon"), async (req, res) => {
-  const data = {};
+    const result = await cloudinary.uploader.upload(req.file.path,{
+        public_id: "event"
+    }).catch((error)=>{console.log(error)});
+    
+    const optimizeUrl = cloudinary.url("shoes", {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+    
+    const autoCropUrl = cloudinary.url("shoes", {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+  
+  let data = {};
   data.name = req.body && req.body.name ? req.body.name : '';
   if(req.file){
-    data.icon = req.file.filename;
+    data.icon = result.url;
   }
-  try {
-    const resp = await axios.post("https://admin-service-ecru.vercel.app/category", data);
+  const finalData = Object.keys(data).length > 0 ? data : {};
+    const resp = await axios.post("http://localhost:4000/category", finalData);
     return res.json(resp.data);
   } catch (error) {
-    res.status(500).send("Error fetching data");
+    res.status(500).send(error);
   }
 });
 export default router;
